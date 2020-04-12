f80ebde6acad2a8ee3f95754a6f764df874885d6
David Merrick <david@david-merrick.com>
2020-04-12 13:14:09 -0700

    Got tests running locally (#3)
    * WIP: Getting tests running with embedded local server.
    
    * Got tests working locally

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # micronaut-quarantinebot
 Trying out a Micronaut project on AWS Lambda.
 
-# Reference:
-https://www.raywenderlich.com/5777183-write-an-aws-lambda-function-with-kotlin-and-micronaut
+# Running tests locally
+
+1. Do a `./gradlew build` to generate kapt annotations
+2. Run tests as usual in IntelliJ
--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,9 @@ plugins {
 
 group = "com.merricklabs.quarantinebot"
 
+val micronautTestVersion by extra("1.1.5")
+val micronautVersion by extra("1.3.4")
+
 application {
     mainClassName = "com.merricklabs.quarantinebot.Application"
     applicationDefaultJvmArgs = listOf("")
@@ -19,13 +22,33 @@ application {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.50")
-    implementation("io.micronaut:micronaut-runtime:1.3.4")
-    implementation("io.micronaut.aws:micronaut-function-aws-api-proxy:1.3.4")
-    kapt(platform("io.micronaut:micronaut-bom:1.3.4"))
+    implementation("io.micronaut:micronaut-runtime:$micronautVersion")
+    implementation("io.micronaut.aws:micronaut-function-aws-api-proxy:$micronautVersion")
+
+    kapt(platform("io.micronaut:micronaut-bom:$micronautVersion"))
     kapt("io.micronaut:micronaut-inject-java")
     kapt("io.micronaut:micronaut-validation")
+
     runtimeOnly("com.amazonaws:aws-lambda-java-log4j2:1.0.0")
     runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:2.9.1")
+
+    // Test
+
+    kaptTest(platform("io.micronaut:micronaut-bom:$micronautVersion"))
+    kaptTest("io.micronaut:micronaut-inject-java:$micronautVersion")
+
+    testImplementation("io.micronaut:micronaut-function-client:$micronautVersion")
+    testImplementation("io.micronaut:micronaut-http-client:$micronautVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.2")
+    testImplementation("io.micronaut.test:micronaut-test-spock:$micronautTestVersion")
+    testImplementation("io.micronaut.test:micronaut-test-kotlintest:$micronautTestVersion")
+    testImplementation("io.micronaut.test:micronaut-test-junit5:$micronautTestVersion")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
+    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.8")
+    testRuntimeOnly("io.micronaut:micronaut-http-server-netty:$micronautVersion")
+    testRuntimeOnly("io.micronaut:micronaut-function-web:$micronautVersion")
 }
 
 tasks {
