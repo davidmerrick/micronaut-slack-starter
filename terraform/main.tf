f7a970d0b9d0747b9c969019f28aceaf9364790d
David Merrick <david@david-merrick.com>
2020-05-10 22:48:20 -0700

    Added s3 and dynamo for backend state storage (#13)

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,43 @@ provider "aws" {
   region     = "us-west-2"
 }
 
+# Remote state
+# Based on https://blog.gruntwork.io/how-to-manage-terraform-state-28f5697e68fa
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "io.github.davidmerrick.quarantinebot.tfstate"
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "quarantinebot-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "io.github.davidmerrick.quarantinebot.tfstate"
+    key            = "global/s3/terraform.tfstate"
+    region = "us-west-2"
+    dynamodb_table = "quarantinebot-locks"
+    encrypt        = true
+  }
+}
+
 # API Gateway
 
 resource "aws_api_gateway_rest_api" "example" {
